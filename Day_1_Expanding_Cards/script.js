c0f9e32f5e14d2c981ea6ce481d8e6fb5fefc1bf
Dakotah Godfrey <dakotah.c.godfrey@gmail.com>
2021-02-02 12:29:59 -0500

    Initial Commit

---
+++ Day_1_Expanding_Cards/script.js
@@ -0,0 +1,18 @@
+const panels = document.querySelectorAll('.panel');
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
+
+panels.forEach(panel => {
+    panel.addEventListener('mouseover', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+    panel.addEventListener('focus',  () => {
+            removeActiveClasses();
+            panel.classList.add('active');
+        })
+})
